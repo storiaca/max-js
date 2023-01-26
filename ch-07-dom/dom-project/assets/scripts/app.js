caaabe54fcf6720bb9cf552d83f3090fbddf9f4c
Aleksandar Ristić <storiaca@gmail.com>
2023-01-26 09:05:16 +0100

    Selecting the modal and add button

---
+++ ch-07-dom/dom-project/assets/scripts/app.js
@@ -0,0 +1,6 @@
+const addMovieModal = document.getElementById("add-modal");
+// const addModal = document.querySelector("#add-modal");
+// const addModal = document.body.children[1];
+
+const startAddMovieButton = document.querySelector("header button");
+//const startAddMovieButton = document.querySelector("header").lastElementChild;
