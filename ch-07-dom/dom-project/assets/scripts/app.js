243bfedd7dfae7b5502c85441b9a552742f40098
Aleksandar Ristić <storiaca@gmail.com>
2023-01-27 08:38:23 +0100

    Controlling the backdrop

--- ch-07-dom/dom-project/assets/scripts/app.js
+++ ch-07-dom/dom-project/assets/scripts/app.js
@@ -5,8 +5,30 @@ const addMovieModal = document.getElementById("add-modal");
 const startAddMovieButton = document.querySelector("header button");
 //const startAddMovieButton = document.querySelector("header").lastElementChild;
 
+const backdrop = document.getElementById("backdrop");
+//const backdrop = document.body.firstElementChild;
+
+const cancelAddMovieButton = addMovieModal.querySelector(".btn--passive");
+
+const toggleBackdrop = () => {
+  backdrop.classList.toggle("visible");
+};
+
 const toggleMovieModal = () => {
   addMovieModal.classList.toggle("visible");
+  toggleBackdrop();
+};
+
+const backdropClickHandler = () => {
+  toggleMovieModal();
+};
+
+const cancelAddMovie = () => {
+  toggleMovieModal();
 };
 
 startAddMovieButton.addEventListener("click", toggleMovieModal);
+
+backdrop.addEventListener("click", backdropClickHandler);
+
+cancelAddMovieButton.addEventListener("click", cancelAddMovie);
