47143f29235beb8c9b9e93a59fc2988c8cb1928f
Aleksandar Ristić <storiaca@gmail.com>
2023-01-28 20:11:15 +0100

    Rendering movie items on the screen

--- ch-07-dom/dom-project/assets/scripts/app.js
+++ ch-07-dom/dom-project/assets/scripts/app.js
@@ -14,7 +14,36 @@ const confirmAddButton = cancelAddMovieButton.nextElementSibling;
 const userInputs = addMovieModal.querySelectorAll("input");
 //const userInputs = addMovieModal.getElementsByTagName("input");
 
+const entryTextSection = document.getElementById("entry-text");
+
 const movies = [];
+
+// https://upload.wikimedia.org/wikipedia/commons/thumb/9/99/Unofficial_JavaScript_logo_2.svg/768px-Unofficial_JavaScript_logo_2.svg.png
+
+const updateUI = () => {
+  if (movies.length === 0) {
+    entryTextSection.style.display = "block";
+  } else {
+    entryTextSection.style.display = "none";
+  }
+};
+
+const renderNewMovieElement = (title, imageUrl, rating) => {
+  const newMovieElement = document.createElement("li");
+  newMovieElement.className = "movie-element";
+  newMovieElement.innerHTML = `
+    <div class="movie-element__image">
+      <img src="${imageUrl}" alt="${title}" />
+    </div>
+    <div class="movie-element__info">
+      <h2>${title}</h2>
+      <p>${title}/5 stars</p>
+    </div>
+  `;
+  const listRoot = document.getElementById("movie-list");
+
+  listRoot.append(newMovieElement);
+};
 
 const toggleBackdrop = () => {
   backdrop.classList.toggle("visible");
@@ -65,9 +94,10 @@ const addMoviehandler = () => {
   movies.push(newMovie);
   console.log(movies);
 
-  clearMovieInputs();
-
   toggleMovieModal();
+  clearMovieInputs();
+  renderNewMovieElement(newMovie.title, newMovie.image, newMovie.rating);
+  updateUI();
 };
 
 startAddMovieButton.addEventListener("click", toggleMovieModal);
