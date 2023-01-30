48ee68d5fd165c6789b833dcf55424c9ef7ccbce
Aleksandar Ristić <storiaca@gmail.com>
2023-01-30 08:48:33 +0100

    Deleting movie elements

--- ch-07-dom/dom-project/assets/scripts/app.js
+++ ch-07-dom/dom-project/assets/scripts/app.js
@@ -28,7 +28,21 @@ const updateUI = () => {
   }
 };
 
-const renderNewMovieElement = (title, imageUrl, rating) => {
+const deleteMovieHandler = (movieId) => {
+  let movieIndex = 0;
+  for (const movie of movies) {
+    if (movie.id === movieId) {
+      break;
+    }
+    movieIndex++;
+  }
+  movies.splice(movieIndex, 1);
+  const listRoot = document.getElementById("movie-list");
+  listRoot.children[movieIndex].remove();
+  //listRoot.removeChild(listRoot.children[movieIndex]);
+};
+
+const renderNewMovieElement = (id, title, imageUrl, rating) => {
   const newMovieElement = document.createElement("li");
   newMovieElement.className = "movie-element";
   newMovieElement.innerHTML = `
@@ -37,9 +51,11 @@ const renderNewMovieElement = (title, imageUrl, rating) => {
     </div>
     <div class="movie-element__info">
       <h2>${title}</h2>
-      <p>${title}/5 stars</p>
+      <p>${rating}/5 stars</p>
     </div>
   `;
+
+  newMovieElement.addEventListener("click", deleteMovieHandler.bind(null, id));
   const listRoot = document.getElementById("movie-list");
 
   listRoot.append(newMovieElement);
@@ -86,6 +102,7 @@ const addMoviehandler = () => {
   }
 
   const newMovie = {
+    id: Math.random().toString(),
     title: titleValue,
     image: imageUrlValue,
     rating: ratingValue,
@@ -96,7 +113,12 @@ const addMoviehandler = () => {
 
   toggleMovieModal();
   clearMovieInputs();
-  renderNewMovieElement(newMovie.title, newMovie.image, newMovie.rating);
+  renderNewMovieElement(
+    newMovie.id,
+    newMovie.title,
+    newMovie.image,
+    newMovie.rating
+  );
   updateUI();
 };
 
