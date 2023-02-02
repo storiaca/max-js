d75a4bac5696bd1c3d41a15dedf70320dab86033
Aleksandar Ristić <storiaca@gmail.com>
2023-02-02 09:05:13 +0100

    push(), pop(), unshift(), shift()
    Adding & Removing Elements

--- ch-08-arrays-iterables/arrays-01/app.js
+++ ch-08-arrays-iterables/arrays-01/app.js
@@ -1,6 +1,6 @@
-const numbers = [1, 2, 3];
+// const numbers = [1, 2, 3];
 
-console.log(numbers);
+// console.log(numbers);
 
 //const moreNumbers = new Array("Hi", "Welcome");
 //const moreNumbers = new Array(5); // Array(5) [ <5 empty slots> ]
@@ -13,24 +13,38 @@ console.log(numbers);
 //const moreNumbers = Array.from("Hi!");
 //console.log(moreNumbers);
 
-const listItems = document.querySelectorAll("li");
-console.log(listItems);
+// const listItems = document.querySelectorAll("li");
+// console.log(listItems);
 
-const arrayListItems = Array.from(listItems);
-console.log(arrayListItems);
+// const arrayListItems = Array.from(listItems);
+// console.log(arrayListItems);
 
-const hobbies = ["Cooking", "Sports"];
-const personalData = [30, "Max", { moreDetail: [] }];
+// const hobbies = ["Cooking", "Sports"];
+// const personalData = [30, "Max", { moreDetail: [] }];
 
-const analyticsData = [
-  [1, 1.6],
-  [-5.4, 2.1],
-];
+// const analyticsData = [
+//   [1, 1.6],
+//   [-5.4, 2.1],
+// ];
 
-for (const data of analyticsData) {
-  for (const dataPoint of data) {
-    console.log(dataPoint);
-  }
-}
+// for (const data of analyticsData) {
+//   for (const dataPoint of data) {
+//     console.log(dataPoint);
+//   }
+// }
 
-console.log(personalData[1]);
+// console.log(personalData[1]);
+
+// push, pop, unshift, shift
+
+const hobbies = ["Sports", "Cooking"];
+
+hobbies.push("Reading"); // add at the end of array, return number
+hobbies.unshift("Coding"); // add at the beginning of array, return number
+const popedValue = hobbies.pop(); // remove last element from array, we can store that element in variable, returns string
+hobbies.shift(); // removes first element from array
+console.log(hobbies);
+
+hobbies[1] = "Coding";
+hobbies[5] = "Reading"; // rarely used
+console.log(hobbies, hobbies[4]);
