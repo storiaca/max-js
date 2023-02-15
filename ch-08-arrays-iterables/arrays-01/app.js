6833aaad34a787f456accacdd4b05a6444a86290
Aleksandar Ristić <storiaca@gmail.com>
2023-02-15 10:50:36 +0100

    The Spread Operator (...) end

--- ch-08-arrays-iterables/arrays-01/app.js
+++ ch-08-arrays-iterables/arrays-01/app.js
@@ -342,4 +342,24 @@ const nameFragments = ["Max", "Schwarz"];
 
 const copiedNameFragments = [...nameFragments];
 
-console.log(copiedNameFragments);
+nameFragments.push("Mr");
+
+console.log(nameFragments, copiedNameFragments);
+
+const prices = [10.99, 5.99, 3.99, 6.59];
+
+console.log(Math.min(...prices));
+
+const persons = [
+  { name: "Max", age: 30 },
+  { name: "Manuel", age: 31 },
+];
+const copiedPersons = persons.map((person) => ({
+  name: person.name,
+  age: person.age,
+}));
+
+persons.push({ name: "Anna", age: 29 });
+persons[0].age = 31;
+
+console.log(persons, copiedPersons);
