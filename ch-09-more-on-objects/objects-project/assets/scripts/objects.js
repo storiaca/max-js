dce8a02c7a0dc4fcd5f86a129ba75a2ef63df209
Aleksandar Ristić <storiaca@gmail.com>
2023-02-26 15:38:48 +0100

    Special Key Names and Square Bracket Property Access

--- ch-09-more-on-objects/objects-project/assets/scripts/objects.js
+++ ch-09-more-on-objects/objects-project/assets/scripts/objects.js
@@ -1,5 +1,10 @@
-const person = {
-  name: "Max",
+const movieList = document.getElementById("movie-list");
+
+movieList.style["background-color"] = "red";
+movieList.style.display = "block";
+
+let person = {
+  "first name": "Max",
   age: 30,
   hobbies: ["Sports", "Cooking"],
   greet: function () {
@@ -12,3 +17,4 @@ delete person.age;
 person.isAdmin = true;
 
 console.log(person);
+console.log(person["first name"]);
