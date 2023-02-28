dd4f5feb4f422e2055588ae753ad58ca232233e3
Aleksandar Ristić <storiaca@gmail.com>
2023-02-28 08:28:16 +0100

    Property Types & Property Order

--- ch-09-more-on-objects/objects-project/assets/scripts/objects.js
+++ ch-09-more-on-objects/objects-project/assets/scripts/objects.js
@@ -10,11 +10,16 @@ let person = {
   greet: function () {
     alert("Hi there!");
   },
+  1.5: "hello",
 };
 
 // person.age = 31;
 delete person.age;
 person.isAdmin = true;
 
-console.log(person);
+
 console.log(person["first name"]);
+
+console.log(person[1.5]);
+
+console.log(person);
