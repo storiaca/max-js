89897a777ebe7f3a34e6feed74f3b93eb4474604
Aleksandar Ristić <storiaca@gmail.com>
2023-02-28 09:56:28 +0100

    Dynamic Property Access
    Setting Properties Dynamically

--- ch-09-more-on-objects/objects-project/assets/scripts/objects.js
+++ ch-09-more-on-objects/objects-project/assets/scripts/objects.js
@@ -3,10 +3,13 @@ const movieList = document.getElementById("movie-list");
 movieList.style["background-color"] = "red";
 movieList.style.display = "block";
 
+const userChosenKeyName = "level";
+
 let person = {
   "first name": "Max",
   age: 30,
   hobbies: ["Sports", "Cooking"],
+  [userChosenKeyName]: "...",
   greet: function () {
     alert("Hi there!");
   },
@@ -17,9 +20,12 @@ let person = {
 delete person.age;
 person.isAdmin = true;
 
+const keyName = "first name";
 
-console.log(person["first name"]);
+console.log(person[keyName]);
 
 console.log(person[1.5]);
+
+console.log(person[userChosenKeyName]);
 
 console.log(person);
