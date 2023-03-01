6b7a6a90aaebe62262e9409dd486aff6faca20f0
Aleksandar Ristić <storiaca@gmail.com>
2023-03-01 09:33:45 +0100

    Demo App start
    Shorthand Property Syntax

--- ch-09-more-on-objects/objects-project/assets/scripts/objects.js
+++ ch-09-more-on-objects/objects-project/assets/scripts/objects.js
@@ -1,31 +1,64 @@
-const movieList = document.getElementById("movie-list");
+// const movieList = document.getElementById("movie-list");
 
-movieList.style["background-color"] = "red";
-movieList.style.display = "block";
+// movieList.style["background-color"] = "red";
+// movieList.style.display = "block";
 
-const userChosenKeyName = "level";
+// const userChosenKeyName = "level";
 
-let person = {
-  "first name": "Max",
-  age: 30,
-  hobbies: ["Sports", "Cooking"],
-  [userChosenKeyName]: "...",
-  greet: function () {
-    alert("Hi there!");
-  },
-  1.5: "hello",
+// let person = {
+//   "first name": "Max",
+//   age: 30,
+//   hobbies: ["Sports", "Cooking"],
+//   [userChosenKeyName]: "...",
+//   greet: function () {
+//     alert("Hi there!");
+//   },
+//   1.5: "hello",
+// };
+
+// // person.age = 31;
+// delete person.age;
+// person.isAdmin = true;
+
+// const keyName = "first name";
+
+// console.log(person[keyName]);
+
+// console.log(person[1.5]);
+
+// console.log(person[userChosenKeyName]);
+
+// console.log(person);
+
+/* ==== App Code ==== */
+const addMovieBtn = document.getElementById("add-movie-btn");
+const searchBtn = document.getElementById("search-btn");
+
+const movies = [];
+
+const addMovieHandler = () => {
+  let title = document.getElementById("title").value;
+  let extraName = document.getElementById("extra-name").value;
+  let extraValue = document.getElementById("extra-value").value;
+
+  if (
+    title.trim() === "" ||
+    extraName.trim() === "" ||
+    extraValue.trim() === ""
+  ) {
+    return;
+  }
+
+  const newMovie = {
+    info: {
+      title,
+      [extraName]: extraValue,
+    },
+    id: Math.random(),
+  };
+
+  movies.push(newMovie);
+  console.log(newMovie);
 };
 
-// person.age = 31;
-delete person.age;
-person.isAdmin = true;
-
-const keyName = "first name";
-
-console.log(person[keyName]);
-
-console.log(person[1.5]);
-
-console.log(person[userChosenKeyName]);
-
-console.log(person);
+addMovieBtn.addEventListener("click", addMovieHandler);
