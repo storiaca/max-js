a27192f6923d2bec114d869906f340356d2aa71e
Aleksandar Ristić <storiaca@gmail.com>
2023-03-08 09:11:11 +0100

    The Method Shorthand Syntax

--- ch-09-more-on-objects/objects-project/assets/scripts/objects.js
+++ ch-09-more-on-objects/objects-project/assets/scripts/objects.js
@@ -134,7 +134,7 @@ const addMovieHandler = () => {
       [extraName]: extraValue,
     },
     id: Math.random().toString(),
-    getFormattedTitle: function () {
+    getFormattedTitle() {
       return this.info.title.toUpperCase();
     },
   };
