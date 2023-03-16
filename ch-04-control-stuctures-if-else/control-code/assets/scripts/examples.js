bc28f1c692341ee8f542a9e6b6ae163ca6943458
Aleksandar Ristić <storiaca@gmail.com>
2023-03-16 08:20:10 +0100

    Beware When Comparing Objects & Arrays for Equality!

--- ch-04-control-stuctures-if-else/control-code/assets/scripts/examples.js
+++ ch-04-control-stuctures-if-else/control-code/assets/scripts/examples.js
@@ -7,3 +7,22 @@
 2 === 2; // true
 
 2 === "2"; // false
+
+/* === Beware When Comparing Objects & Arrays for Equality! === */
+const person1 = { name: "Max" };
+
+const person2 = { name: "Max" };
+
+person1 === person2; // false
+
+person1.name === person2.name; // true
+
+const person3 = person1;
+
+person3 === person1; // true
+
+const hobbies = ["Sports", "Cooking"];
+
+const moreHobbies = ["Sports", "Cooking"];
+
+hobbies === moreHobbies; // false
