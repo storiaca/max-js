ae2ee5c4f211d9342d07e4ba8930638f5084fcce
Aleksandar Ristić <storiaca@gmail.com>
2023-04-21 08:12:42 +0200

    Controlling Iterations with "continue"

--- ch-04-control-stuctures-if-else/control-code/assets/scripts/examples.js
+++ ch-04-control-stuctures-if-else/control-code/assets/scripts/examples.js
@@ -75,3 +75,11 @@ while (!finished) {
     console.log(randomNumbers);
   }
 }
+
+/* === Controlling Iterations with "continue" === */
+for (let i = 0; i < 5; i++) {
+  if (i === 3) {
+    continue;
+  }
+  console.log(i); // 0,1,2,4,5
+}
