bb202aa3267c2d29aa2e8e9f017487abb0dac5e7
Aleksandar Ristić <storiaca@gmail.com>
2023-04-26 08:45:39 +0200

    Working with "try-catch" to Catch & Handle Errors

--- ch-04-control-stuctures-if-else/monster-killer/assets/scripts/app.js
+++ ch-04-control-stuctures-if-else/monster-killer/assets/scripts/app.js
@@ -24,7 +24,17 @@ function getMaxLifeValues() {
   return parsedValue;
 }
 
-let chosenMaxLife = getMaxLifeValues();
+let chosenMaxLife;
+try {
+  chosenMaxLife = getMaxLifeValues();
+} catch (error) {
+  console.log(error);
+  chosenMaxLife = 100;
+  alert("You entered something wrong, default value of 100 was used.");
+  //throw error;
+}
+// finally {
+// }
 
 let currentMonsterHealth = chosenMaxLife;
 let currentPlayerHealth = chosenMaxLife;
