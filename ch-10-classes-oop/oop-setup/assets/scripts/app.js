a0ee79f291373b555ab7d7618719929c881879aa
Aleksandar Ristic <storiaca@gmail.com>
2023-05-09 08:25:37 +0200

    Defining aand using a first class

--- ch-10-classes-oop/oop-setup/assets/scripts/app.js
+++ ch-10-classes-oop/oop-setup/assets/scripts/app.js
@@ -1,5 +1,15 @@
+class Product {
+  title = "DEFAULT";
+  imageUrl;
+  description;
+  price;
+}
+
+console.log(new Product());
+
 const productList = {
   products: [
+    //new Product()
     {
       title: "A Pillow",
       imageUrl:
