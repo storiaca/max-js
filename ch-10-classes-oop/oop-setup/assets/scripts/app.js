55513a5288d782b2c37e8d0ac5f7266a42577aa3
Aleksandar Ristic <storiaca@gmail.com>
2023-05-13 13:02:20 +0200

    Adding a cart and shop class

--- ch-10-classes-oop/oop-setup/assets/scripts/app.js
+++ ch-10-classes-oop/oop-setup/assets/scripts/app.js
@@ -12,6 +12,20 @@ class Product {
   }
 }
 
+class ShoppingCart {
+  items = [];
+
+  render() {
+    const cartEl = document.createElement("section");
+    cartEl.innerHTML = `
+      <h2>Total:\$${0}</h2>
+      <button>Order Now!</button>
+    `;
+    cartEl.className = "cart";
+    return cartEl;
+  }
+}
+
 class ProductItem {
   constructor(product) {
     this.product = product;
@@ -61,7 +75,6 @@ class ProductList {
   constructor() {}
 
   render() {
-    const renderHook = document.getElementById("app");
     const prodList = document.createElement("ul");
     prodList.className = "product-list";
     for (const prod of this.products) {
@@ -69,9 +82,24 @@ class ProductList {
       const prodEl = productItem.render();
       prodList.append(prodEl);
     }
+
+    return prodList;
+  }
+}
+
+class Shop {
+  render() {
+    const renderHook = document.getElementById("app");
+
+    const cart = new ShoppingCart();
+    const cartEl = cart.render();
+    const productList = new ProductList();
+    const prodList = productList.render();
+
+    renderHook.append(cartEl);
     renderHook.append(prodList);
   }
 }
 
-const productList = new ProductList();
-productList.render();
+const shop = new Shop();
+shop.render();
