a55073cb00e3e8fd8fba56e1d9ac73f9dd767b9e
Aleksandar Ristic <storiaca@gmail.com>
2023-05-19 09:30:06 +0200

    Overriding methods and the super() constructor

--- ch-10-classes-oop/oop-setup/assets/scripts/app.js
+++ ch-10-classes-oop/oop-setup/assets/scripts/app.js
@@ -22,7 +22,10 @@ class ElementAttribute {
 class Component {
   constructor(renderHookId) {
     this.hookId = renderHookId;
+    this.render();
   }
+
+  render() {}
 
   createRootElement(tag, cssClasses, attributes) {
     const rootElement = document.createElement(tag);
@@ -130,18 +133,19 @@ class ProductList extends Component {
       new ElementAttribute("id", "prod-list"),
     ]);
     for (const prod of this.products) {
-      const productItem = new ProductItem(prod, "prod-list");
-      productItem.render();
+      new ProductItem(prod, "prod-list");
     }
   }
 }
 
 class Shop {
+  constructor() {
+    this.render();
+  }
+
   render() {
     this.cart = new ShoppingCart("app");
-    this.cart.render();
-    const productList = new ProductList("app");
-    productList.render();
+    new ProductList("app");
   }
 }
 
@@ -150,7 +154,6 @@ class App {
 
   static init() {
     const shop = new Shop();
-    shop.render();
     this.cart = shop.cart;
   }
 
