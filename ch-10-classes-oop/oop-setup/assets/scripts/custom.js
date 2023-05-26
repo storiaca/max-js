d4d44260fc2e5d7463dbdc35911fd7118f6b786f
Aleksandar Ristic <storiaca@gmail.com>
2023-05-26 08:03:43 +0200

    Built-in Classes

--- ch-10-classes-oop/oop-setup/assets/scripts/custom.js
+++ ch-10-classes-oop/oop-setup/assets/scripts/custom.js
@@ -1,19 +1,31 @@
 /* The "instanceof" Operator */
 
-class Person {
-  name = "Max";
-}
+// class Person {
+//   name = "Max";
+// }
 
-const p = new Person();
+// const p = new Person();
 
-console.log(typeof p); // object
+// console.log(typeof p); // object
 
-console.log(p instanceof Person); // true
+// console.log(p instanceof Person); // true
 
-const btn = document.querySelector("button");
+// const btn = document.querySelector("button");
 
-console.dir(btn);
+// console.dir(btn);
 
-console.log(btn instanceof HTMLButtonElement); // true
+// console.log(btn instanceof HTMLButtonElement); // true
 
-console.log(btn instanceof HTMLElement); // true
+// console.log(btn instanceof HTMLElement); // true
+
+/* Built-in Classes */
+
+const obj = new Object();
+
+// ovo gore je isto kao i da smo napisali ovo
+
+const obj2 = {};
+
+// samo sto je ovo drugo brze
+
+// new Array() etc..
