ea4bf8da4010bfb901b7924983250122d64a2e46
Aleksandar Ristic <storiaca@gmail.com>
2023-06-26 08:24:09 +0200

    Setting timers and intervals

--- ch-13-dom-manipulation/adv-dom-01-dataset/assets/scripts/app.js
+++ ch-13-dom-manipulation/adv-dom-01-dataset/assets/scripts/app.js
@@ -176,9 +176,16 @@ class App {
     // someScript.textContent = 'alert("Hi there")';
     // document.head.append(someScript);
 
+    // document
+    //   .getElementById("start-analytics-btn")
+    //   .addEventListener("click", this.startAnalytics);
+
+    const timerId = setTimeout(this.startAnalytics, 3000);
     document
-      .getElementById("start-analytics-btn")
-      .addEventListener("click", this.startAnalytics);
+      .getElementById("stop-analytics-btn")
+      .addEventListener("click", () => {
+        clearTimeout(timerId);
+      });
   }
 
   static startAnalytics() {
