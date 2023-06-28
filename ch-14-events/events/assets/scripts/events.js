9e52873ab322f1bb39bcb3ccbb956ba28e3fd4a2
Aleksandar Ristic <storiaca@gmail.com>
2023-06-28 08:31:06 +0200

    Different Ways of Listening to Events

---
+++ ch-14-events/events/assets/scripts/events.js
@@ -0,0 +1,23 @@
+const button = document.querySelector("button");
+
+// button.onclick = function () {
+//   console.log("Hello");
+// };
+
+// const buttonClickHandler = () => {
+//   alert("Button was clicked");
+// };
+
+// const anotherButtonClickHandler = () => {
+//   console.log("This was clicked");
+// };
+
+// button.onclick = buttonClickHandler;
+
+// button.onclick = anotherButtonClickHandler;
+
+// this won't work because we can't add more click events on button with onclick, only second function will be execute
+
+button.addEventListener();
+
+button.removeEventListener();
