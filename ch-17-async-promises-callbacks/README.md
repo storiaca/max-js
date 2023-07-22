c3b93bf85a19a93dcd176ac2f52f21fc07cf65ef
Aleksandar Ristic <storiaca@gmail.com>
2023-07-22 12:16:26 +0200

    Blocking code and event loop

--- ch-17-async-promises-callbacks/README.md
+++ ch-17-async-promises-callbacks/README.md
@@ -1,1 +1,5 @@
 # Section 17: Async JavaScript: Promises & Callbacks
+
+## Event Loop
+
+https://developer.mozilla.org/en-US/docs/Web/JavaScript/Event_loop
--- ch-17-async-promises-callbacks/async/app.js
+++ ch-17-async-promises-callbacks/async/app.js
@@ -1,8 +1,18 @@
-const button = document.querySelector('button');
-const output = document.querySelector('p');
+const button = document.querySelector("button");
+const output = document.querySelector("p");
 
 function trackUserHandler() {
-  console.log('Clicked!');
+  console.log("Clicked!");
 }
 
-button.addEventListener('click', trackUserHandler);
+button.addEventListener("click", trackUserHandler);
+
+let result = 0;
+
+for (let i = 0; i < 100000000; i++) {
+  result += i;
+}
+
+console.log(result);
+
+// event loop is part of the browser
