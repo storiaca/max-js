b8a6c63c76f9a7eab6192f27fc8772177287c23c
Aleksandar Ristic <storiaca@gmail.com>
2023-07-27 08:32:25 +0200

    Asyn await error handling

--- ch-17-async-promises-callbacks/async/app.js
+++ ch-17-async-promises-callbacks/async/app.js
@@ -28,8 +28,14 @@ const setTimer = (duration) => {
 
 async function trackUserHandler() {
   //let positonData;
-  const posData = await getPosition();
-  const timerData = await setTimer(2000);
+  let posData;
+  let timerData;
+  try {
+    posData = await getPosition();
+    timerData = await setTimer(2000);
+  } catch (error) {
+    console.log(error);
+  }
   console.log(timerData, posData);
   // .then((posData) => {
   //   positonData = posData;
