0d4c1bca7f41c5b5e643c9f8b1d8ba0bbddbeb76
Aleksandar Ristic <storiaca@gmail.com>
2023-08-03 08:52:58 +0200

    Using the fetch() API

--- ch-18-http-request/README.md
+++ ch-18-http-request/README.md
@@ -3,3 +3,7 @@
 ## JSON Placeholder
 
 https://jsonplaceholder.typicode.com/
+
+## Using XMLHttpRequest
+
+https://developer.mozilla.org/en-US/docs/Web/API/XMLHttpRequest/Using_XMLHttpRequest
--- ch-18-http-request/http-project/assets/scripts/app.js
+++ ch-18-http-request/http-project/assets/scripts/app.js
@@ -5,29 +5,29 @@ const fetchButton = document.querySelector("#available-posts button");
 const postList = document.querySelector("ul");
 
 function sendHttpRequest(method, url, data) {
-  const promise = new Promise((resolve, reject) => {
-    const xhr = new XMLHttpRequest();
-
-    xhr.open(method, url);
-
-    xhr.responseType = "json";
-
-    xhr.onload = function () {
-      if (xhr.status >= 200 && xhr.status < 300) {
-        resolve(xhr.response);
-      } else {
-        reject(new Error("Something went wrong!"));
-      }
-      // const listOfPosts = JSON.parse(xhr.response);
-    };
-
-    xhr.onerror = function () {
-      reject(new Error("Failed to send request!"));
-    };
-
-    xhr.send(JSON.stringify(data));
+  //const promise = new Promise((resolve, reject) => {
+  //   const xhr = new XMLHttpRequest();
+  //   xhr.open(method, url);
+  //   xhr.responseType = "json";
+  //   xhr.onload = function () {
+  //     if (xhr.status >= 200 && xhr.status < 300) {
+  //       resolve(xhr.response);
+  //     } else {
+  //       reject(new Error("Something went wrong!"));
+  //     }
+  //     // const listOfPosts = JSON.parse(xhr.response);
+  //   };
+  //   xhr.onerror = function () {
+  //     reject(new Error("Failed to send request!"));
+  //   };
+  //   xhr.send(JSON.stringify(data));
+  // });
+  // return promise;
+  return fetch(url).then((response) => {
+    // response.text()
+    // response.blob() // for downloading a file
+    return response.json();
   });
-  return promise;
 }
 
 // function fetchPosts() {
