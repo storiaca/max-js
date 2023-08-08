00e3a76a71882bf35b08081cf326f751386a1309
Aleksandar Ristic <storiaca@gmail.com>
2023-08-08 09:32:28 +0200

    Working wih FormData

--- ch-18-http-request/http-project/assets/scripts/app.js
+++ ch-18-http-request/http-project/assets/scripts/app.js
@@ -26,10 +26,11 @@ function sendHttpRequest(method = "GET", url, data) {
   // return promise;
   return fetch(url, {
     method: method,
-    body: JSON.stringify(data),
-    headers: {
-      "Content-Type": "application/json",
-    },
+    // body: JSON.stringify(data),
+    body: data,
+    // headers: {
+    //   "Content-Type": "application/json",
+    // },
   })
     .then((response) => {
       // response.text()
@@ -91,7 +92,14 @@ async function createPost(title, content) {
     userId: userId,
   };
 
-  sendHttpRequest("POST", "https://jsonplaceholder.typicode.com/posts", post);
+  //sendHttpRequest("POST", "https://jsonplaceholder.typicode.com/posts", post);
+
+  const fd = new FormData(form);
+  // fd.append("title", title);
+  // fd.append("body", content);
+  fd.append("userId", userId);
+
+  sendHttpRequest("POST", "https://jsonplaceholder.typicode.com/posts", fd);
 }
 // "https://jsonplaceholder.typicode.com/posts"
 
