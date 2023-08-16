31e2a69b4912311d9a8499605d6affe27c708806
Aleksandar Ristic <storiaca@gmail.com>
2023-08-16 09:22:19 +0200

    More named export syntax variations multiple example

--- ch-20-modular-js/modules/assets/scripts/App/ProjectList.js
+++ ch-20-modular-js/modules/assets/scripts/App/ProjectList.js
@@ -1,5 +1,5 @@
 import { ProjectItem } from "./ProjectItem.js";
-import { moveElement, DOMHelper } from "../Utility/DOMHelper.js";
+import * as DOMH from "../Utility/DOMHelper.js";
 
 export class ProjectList {
   projects = [];
@@ -58,7 +58,7 @@ export class ProjectList {
 
   addProject(project) {
     this.projects.push(project);
-    moveElement(project.id, `#${this.type}-projects ul`);
+    DOMH.moveElement(project.id, `#${this.type}-projects ul`);
     project.update(this.switchProject.bind(this), this.type);
   }
 
