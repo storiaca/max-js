299c9cc3f581cd31e3dd282647a53f6ff409380a
Aleksandar Ristic <storiaca@gmail.com>
2023-08-21 21:53:37 +0200

    Configure eslint start

--- ch-21-js-tooling/tooling/assets/scripts/app.js
+++ ch-21-js-tooling/tooling/assets/scripts/app.js
@@ -1,11 +1,11 @@
-import { ProjectList } from './App/ProjectList.js';
+import { ProjectList } from "./App/ProjectList.js";
 
-globalThis.DEFAULT_VALUE = 'MAX';
+globalThis.DEFAULT_VALUE = "MAX";
 
 class App {
   static init() {
-    const activeProjectsList = new ProjectList('active');
-    const finishedProjectsList = new ProjectList('finished');
+    const activeProjectsList = new ProjectList("active");
+    const finishedProjectsList = new ProjectList("finished");
     activeProjectsList.setSwitchHandlerFunction(
       finishedProjectsList.addProject.bind(finishedProjectsList)
     );
@@ -21,11 +21,13 @@ class App {
   }
 
   static startAnalytics() {
-    const analyticsScript = document.createElement('script');
-    analyticsScript.src = 'assets/scripts/Utility/Analytics.js';
+    const analyticsScript = document.createElement("script");
+    analyticsScript.src = "assets/scripts/Utility/Analytics.js";
     analyticsScript.defer = true;
     document.head.append(analyticsScript);
   }
 }
 
 App.init();
+// komanda ctrl + shift + p za otvaranje konfiguracije u vs code-u
+// komanda za konfigurisanje es-lint-a: npx eslint --init
