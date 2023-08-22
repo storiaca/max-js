abaae4f3dda775982cd73bc099fc8db528b9ac5b
Aleksandar Ristic <storiaca@gmail.com>
2023-08-22 08:57:02 +0200

    Linting with eslint

--- ch-21-js-tooling/tooling/assets/scripts/app.js
+++ ch-21-js-tooling/tooling/assets/scripts/app.js
@@ -1,16 +1,17 @@
-import { ProjectList } from "./App/ProjectList.js";
+// eslint-disable-next-line import/extensions
+import { ProjectList } from './App/ProjectList.js';
 
-globalThis.DEFAULT_VALUE = "MAX";
+globalThis.DEFAULT_VALUE = 'MAX';
 
 class App {
   static init() {
-    const activeProjectsList = new ProjectList("active");
-    const finishedProjectsList = new ProjectList("finished");
+    const activeProjectsList = new ProjectList('active');
+    const finishedProjectsList = new ProjectList('finished');
     activeProjectsList.setSwitchHandlerFunction(
-      finishedProjectsList.addProject.bind(finishedProjectsList)
+      finishedProjectsList.addProject.bind(finishedProjectsList),
     );
     finishedProjectsList.setSwitchHandlerFunction(
-      activeProjectsList.addProject.bind(activeProjectsList)
+      activeProjectsList.addProject.bind(activeProjectsList),
     );
 
     // const timerId = setTimeout(this.startAnalytics, 3000);
@@ -21,8 +22,8 @@ class App {
   }
 
   static startAnalytics() {
-    const analyticsScript = document.createElement("script");
-    analyticsScript.src = "assets/scripts/Utility/Analytics.js";
+    const analyticsScript = document.createElement('script');
+    analyticsScript.src = 'assets/scripts/Utility/Analytics.js';
     analyticsScript.defer = true;
     document.head.append(analyticsScript);
   }
