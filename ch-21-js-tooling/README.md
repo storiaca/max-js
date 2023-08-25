3047536d729483c33b2f8ede12e432d4d11f73d9
Aleksandar Ristic <storiaca@gmail.com>
2023-08-25 08:51:29 +0200

    Generating sourcemaps

---
+++ ch-21-js-tooling/README.md
@@ -0,0 +1,9 @@
+# Section 21: JavaScript Tooling & Workflows
+
+## Wepack
+
+https://webpack.js.org/
+
+## Webpack devtool
+
+https://webpack.js.org/configuration/devtool/
--- ch-21-js-tooling/tooling/webpack.config.js
+++ ch-21-js-tooling/tooling/webpack.config.js
@@ -8,6 +8,7 @@ module.exports = {
     path: path.resolve(__dirname, 'assets', 'scripts'),
     // publicPath: 'assets/scripts',
   },
+  devtool: 'cheap-module-source-map',
   devServer: {
     static: {
       directory: path.join(__dirname),
