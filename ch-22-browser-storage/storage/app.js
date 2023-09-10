a3c3f989223c3eff4289476a3aa0b3d4f396564f
Aleksandar Ristic <storiaca@gmail.com>
2023-09-10 12:20:10 +0200

    Cookies

--- ch-22-browser-storage/storage/app.js
+++ ch-22-browser-storage/storage/app.js
@@ -1,24 +1,9 @@
 const storeBtn = document.getElementById('store-btn');
 const retrBtn = document.getElementById('retrieve-btn');
 
-const userId = 'u123';
-const user = {
-  name: 'Max',
-  age: 33,
-  hobbies: ['Sports', 'Cooking'],
-};
-
 storeBtn.addEventListener('click', () => {
-  sessionStorage.setItem('uid', userId);
-  localStorage.setItem('user', JSON.stringify(user));
+  const userId = 'u123';
+  document.cookie = `uid=${userId}`;
 });
-retrBtn.addEventListener('click', () => {
-  const extarctedId = sessionStorage.getItem('uid');
-  const extractedUser = JSON.parse(localStorage.getItem('user'));
-  console.log(extractedUser);
-  if (extarctedId) {
-    console.log('Got the id - ' + extarctedId);
-  } else {
-    console.log('Could not find id.');
-  }
-});
+retrBtn.addEventListener('click', () => {});
+console.log(document.cookie);
