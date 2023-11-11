e96739e33152806c30c0cd49be71bae2aaa64115
Aleksandar Ristic <storiaca@gmail.com>
2023-11-11 12:21:44 +0100

    Understanding iterators

--- ch-026-meta-programming/meta/app.js
+++ ch-026-meta-programming/meta/app.js
@@ -1,22 +1,47 @@
 // Library land
-const uid = Symbol('uid');
+const uid = Symbol("uid");
 
 console.log(uid);
 
 const user = {
   //id: 'p1',
-  [uid]: 'p1',
-  name: 'Max',
+  [uid]: "p1",
+  name: "Max",
   age: 30,
-  [Symbol.toStringTag]: 'User'
-}
+  [Symbol.toStringTag]: "User",
+};
 
-user[uid] = 'p3';
+user[uid] = "p3";
 
 // app land => Using the library
 
-user.id = 'p2'; // this should not be possible!
+user.id = "p2"; // this should not be possible!
 
-console.log(Symbol('uid') === Symbol('uid')); // false
+console.log(Symbol("uid") === Symbol("uid")); // false
 
 console.log(user.toString());
+
+const company = {
+  curEmployee: 0,
+  employees: ["Max", "Manu", "Anna"],
+  next() {
+    if (this.curEmployee >= this.employees.length) {
+      return { value: this.curEmployee, done: true };
+    }
+    const returnValue = {
+      value: this.employees[this.curEmployee],
+      done: false,
+    };
+    this.curEmployee++;
+    return returnValue;
+  },
+};
+
+let employee = company.next();
+
+while (!employee.done) {
+  console.log(employee.value);
+  employee = company.next();
+}
+
+console.log(employee);
